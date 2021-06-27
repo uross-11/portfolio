4186f5408194a7e1acbc589cf95dd532afe93f48
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-06-27 16:51:46 +0200

    Modal tablet and desktop responsive

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -12,7 +12,7 @@ const ProjectModal = () => {
         <button className='projectmodal__exit' onClick={closeModal}>close</button>
         <img src={modal.img} alt={`${modal.title} image`} />
       </div>
-      <div className="projectmodal__content container">
+      <div className="projectmodal__content">
         <h2 className='projectmodal__title'>{modal.title}</h2>
         <p className='projectmodal__description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem iure dolorum nobis ut tempora eveniet. Unde, incidunt ratione ad laboriosam amet magnam aspernatur ipsa distinctio exercitationem ut dicta minima at?</p>
         <div className='projectmodal__tech'>
