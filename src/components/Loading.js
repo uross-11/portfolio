a49660101a01c0f3f5583ef34baf73dd8b7eb0d2
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2022-03-13 01:43:56 +0100

    Fix entrance animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='loading'>
-     <div className="loading__inner"></div>
+     <div className="loading__inner">
+       <div className='loading__inner__a'>
+        <div className="loading__inner__a__b"></div>
+       </div>
+     </div>
     </div>
   );
 }
