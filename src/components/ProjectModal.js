2ea193ffcb4a8968a48d4962292ee82f9555f635
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2022-03-17 02:34:19 +0100

    Add project modal animation

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useGlobalContext } from '../context';
 import { data } from '../data';
-import { useSpring, animated } from 'react-spring';
 
 const ProjectModal = () => {
   const { projectId, closeModal } = useGlobalContext();
   const modal = data.filter(item => item.id === projectId)[0]
-  const props = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 }
-  })
+  const modalRef = useRef();
+
+  const handleExit = () => {
+    modalRef.current.style.opacity = '0';
+    setTimeout(() => {
+      closeModal();
+    }, 300);
+  }
+
+  useEffect(() => {
+    modalRef.current.style.opacity = '1';
+  }, [])
 
   return (
-    <animated.div style={props} className='projectmodal'>
+    <div ref={modalRef} className='projectmodal'>
       <div className='projectmodal__image'>
-        <button className='projectmodal__exit' onClick={closeModal}>close</button>
+        <button className='projectmodal__exit' onClick={handleExit}>close</button>
         <img src={modal.img} alt={`${modal.title} img`} />
       </div>
       <div className="projectmodal__content">
         <h2 className='projectmodal__title'>{modal.title}</h2>
         <p className='projectmodal__description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem iure dolorum nobis ut tempora eveniet. Unde, incidunt ratione ad laboriosam amet magnam aspernatur ipsa distinctio exercitationem ut dicta minima at?</p>
         <div className='projectmodal__tech'>
-          {modal.techstack.map(item => {
-            return <span>{item}</span>
+          {modal.techstack.map((item, index) => {
+            return <span key={index}>{item}</span>
           })}
         </div>
         <div className="projectmodal__url">
@@ -30,7 +37,7 @@ const ProjectModal = () => {
           <div className='projectmodal__url__source'>{modal.sourceurl}</div>
         </div>
       </div>
-    </animated.div>
+    </div>
   );
 }
 
